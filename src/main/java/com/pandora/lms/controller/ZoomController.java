10eb6b9bd2a11087f5d46e333e28d8bc30b6b265
Dori <kingdori902@naver.com>
2023-04-11 15:45:30 +0900

    zoom api

--- src/main/java/com/pandora/lms/controller/ZoomController.java
+++ src/main/java/com/pandora/lms/controller/ZoomController.java
@@ -16,16 +16,9 @@ public class ZoomController {
     @GetMapping("/zoom/gettoken")/*토큰 발급 받고 코드 값을 가져옴*/
     public String get_token(@RequestParam("code") String code) {
 
+
         String zoomUrl = "https://zoom.us/oauth/token";
-
-        FormBody formBody = new FormBody.Builder()
-                .add("code", code) // 1단계에서 받은 code 값
-                .add("redirect_uri", "http://localhost/_new/support/reservation/zoomApi") //등록 된 uri
-                .add("grant_type", "authorization_code") // 문서에 명시 된 grant_type
-                .add("code_verifier", DecEncUtil.encode(code)) // code를 SHA-256 방식으로 암호화하여 전달
-                .build();
-
-        System.err.println(code);
+        System.err.println(code);/*코드값 정상 출력*/
 
         return "zoom";
     }
