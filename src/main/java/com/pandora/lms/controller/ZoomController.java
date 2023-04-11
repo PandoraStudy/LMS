1243a37999fbc566533b1b6e65b2f538da079611
Dori <kingdori902@naver.com>
2023-04-11 16:20:22 +0900

    zoom api 구현중

--- src/main/java/com/pandora/lms/controller/ZoomController.java
+++ src/main/java/com/pandora/lms/controller/ZoomController.java
@@ -1,9 +1,19 @@
 package com.pandora.lms.controller;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kotlin.jvm.internal.TypeReference;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class ZoomController {
@@ -13,14 +23,41 @@ public class ZoomController {
         return "zoom";
     }
 
-    @GetMapping("/zoom/gettoken")/*토큰 발급 받고 코드 값을 가져옴*/
-    public String get_token(@RequestParam("code") String code) {
+    @GetMapping("/zoom/token")/*토큰 발급 받고 코드 값을 가져옴*/
+    public String get_token(@RequestParam("code") String code) throws IOException {
+        OkHttpClient client = new OkHttpClient(); /*통신을 위한 OkHttp*/
+        ObjectMapper mapper = new ObjectMapper();/*Json 처리를 위하여 생성*/
+
+        String zoomUrl = "https://zoom.us/oauth/token"; //Access token 을 받는 zoom api 호출 url
+
+        FormBody formBody = new FormBody.Builder()/*http 요청 바디를 만듬*/
+                .add("code", code) // 1단계에서 받은 code 값
+                .add("redirect_uri", "http://localhost/zoom/token") //등록 된 uri
+                .add("grant_type", "authorization_code") // 문서에 명시 된 grant_type
+                /*.add("code_verifier", DecEncUtil.encode(code))*/ // 나중에 시간이 된다면 code를 SHA-256 방식으로 암호화하여 전달할 예정
+                .build();
+
+        Request zoomRequest = new Request.Builder()/*http 요청을 생성하기 위해 만듬*/
+                .url(zoomUrl) // 호출 url
+                .addHeader("Content-Type", "application/x-www-form-urlencoded") // 공식 문서에 명시 된 type
+                .addHeader("Authorization", "Basic") // Client_ID:Client_Secret 을  Base64-encoded 한 값
+                .post(formBody)
+                .build();
 
 
-        String zoomUrl = "https://zoom.us/oauth/token";
-        System.err.println(code);/*코드값 정상 출력*/
+        Response zoomResponse = client.newCall(zoomRequest).execute();/*client를 사용하여 zoomRequest를 서버로 보내고 응답 받은 내용을  zoomResponse에 저장*/
+        String zoomText = zoomResponse.body().string();/*zoomResponse 응답 받은 내용을 문자열로 바꿔줌*/
 
-        return "zoom";
+
+        mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);/*위에서 생성한  ObjectMapper에 벨류값을 배열 타입으로 만들어줌*/
+
+        List<Object> list = mapper.readValue(zoomText, new TypeReference<List<Object>>() {});
+        System.out.println("response :: " + list);
+        model.addAttribute("response", list.get(0));
+
+
+        model.addAttribute("code", code);
+        return "";
     }
 
 
