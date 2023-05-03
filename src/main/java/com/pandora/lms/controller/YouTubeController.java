1bb9ea3a81874debf40c962feb4baed65c270728
심동영 <sdy5188@gmail.com>
2023-05-03 14:36:58 +0900

    동영

--- src/main/java/com/pandora/lms/controller/YouTubeController.java
+++ src/main/java/com/pandora/lms/controller/YouTubeController.java
@@ -1,22 +1,27 @@
 package com.pandora.lms.controller;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpSession;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import com.google.api.client.auth.oauth2.Credential;
 import com.pandora.lms.service.YoutubeService;
 import com.pandora.lms.ytbUtil.OAuth;
 import lombok.AllArgsConstructor;
-import org.apache.ibatis.session.SqlSession;
-import org.json.JSONException;
-import org.apache.commons.io.FilenameUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpSession;
-import java.io.File;
-import java.util.*;
 
 @Controller
 @AllArgsConstructor
